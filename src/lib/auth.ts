a8b6f0beace5f408ea0e10e65bf623d6785153ee
baananou <bananoumohamed@live.fr>
2023-04-19 23:29:34 +0100

    github auth Done

---
+++ src/lib/auth.ts
@@ -0,0 +1,78 @@
+import { NextAuthOptions } from "next-auth";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import { db } from "./db";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+// function getGoogleCred() {
+// 	const clientId = process.env.GOOGLE_CLIENT_ID;
+// 	const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+// 	if (!clientId || clientId.length === 0) {
+// 		throw new Error("Missing Google GOOGLE_CLIENT_ID");
+// 	}
+// 	if (!clientSecret || clientSecret.length === 0) {
+// 		throw new Error("Missing Google GOOGLE_CLIENT_SECRET");
+// 	}
+// 	return { clientId, clientSecret };
+// }
+
+function getGithubCred() {
+	const clientId = process.env.GITHUB_ID;
+	const clientSecret = process.env.GITHUB_SECRET;
+
+	if (!clientId || clientId.length === 0) {
+		throw new Error("Missing GITHUB GITHUB_ID");
+	}
+	if (!clientSecret || clientSecret.length === 0) {
+		throw new Error("Missing GITHUB GITHUB_SECRET");
+	}
+	return { clientId, clientSecret };
+}
+
+export const authOptions: NextAuthOptions = {
+	adapter: UpstashRedisAdapter(db),
+	session: {
+		strategy: "jwt",
+	},
+	pages: {
+		signIn: "/login",
+	},
+	providers: [
+		// GoogleProvider({
+		// 	clientId: getGoogleCred().clientId,
+		// 	clientSecret: getGoogleCred().clientSecret,
+		// }),
+		GitHubProvider({
+			clientId: "Iv1.47b26042470550aa",
+			clientSecret: "a70ac1c5d2c4a02c55772b9124a92f24c5d763b4",
+		}),
+	],
+	callbacks: {
+		async jwt({ token, user }) {
+			const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+			if (!dbUser) {
+				token.id = user!.id;
+				return token;
+			}
+			return {
+				id: dbUser.id,
+				name: dbUser.name,
+				email: dbUser.email,
+				picture: dbUser.image,
+			};
+		},
+		async session({ session, token }) {
+			if (token) {
+				session.user.id = token.id;
+				session.user.email = token.email;
+				session.user.name = token.name;
+				session.user.image = token.picture;
+			}
+			return session;
+		},
+		redirect() {
+			return "/dashboard";
+		},
+	},
+};
---
+++ src/lib/db.ts
@@ -0,0 +1,7 @@
+import { Redis } from '@upstash/redis'
+
+export const db = new Redis({
+  url: 'https://eu2-robust-man-31107.upstash.io',
+  token: 'AXmDASQgMmEwMmE3MTEtNTBiNC00MGZjLTlhNDQtMWRkMzMwNDcyY2NmYjZmNzhiZDdkNmU2NDk4N2FkZmY3ZDE5ZTdkZDk1NzI=',
+})
+   
